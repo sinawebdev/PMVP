1dd98408d7d912b57fa6029dd3760abf4afaa94d
Chrisnat Developer <developer@chrisnat.local>
2026-05-20 13:01:13 +0100

    Use Gunicorn when started by Render

--- run.py
+++ run.py
@@ -5,8 +5,12 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
+    port = os.getenv("PORT", "5000")
+    if os.getenv("RENDER") == "true":
+        os.execvp("gunicorn", ["gunicorn", "run:app", "--bind", f"0.0.0.0:{port}"])
+
     app.run(
         host="0.0.0.0",
-        port=int(os.getenv("PORT", "5000")),
+        port=int(port),
         debug=os.getenv("FLASK_DEBUG", "false").lower() == "true",
     )
